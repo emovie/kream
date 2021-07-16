2521c0d88bc95ea9b1ae6d8260270675f5f60e82
emovie <86423915+emovie@users.noreply.github.com>
2021-07-16 21:01:08 +0900

    Add files via upload

---
+++ kream/kreamSub/src/main/java/com/project/controller/AjaxController.java
@@ -0,0 +1,33 @@
+package com.project.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project.model.ProductDTO;
+import com.project.service.MainpageService;
+
+@RestController
+public class AjaxController {
+
+	@Autowired private MainpageService ms;
+	
+	@GetMapping("/search/{keyword}/")
+	public List<ProductDTO> search(@PathVariable String keyword) {
+		return ms.search(keyword);
+	}
+	
+	@GetMapping("/main/justDropped/")
+	public List<ProductDTO> justDropped() {
+		return ms.justDropped();
+	}
+	
+	@GetMapping("/main/mostPopular/")
+	public List<ProductDTO> mostPopular() {
+		return ms.mostPopular();
+	}
+
+}
