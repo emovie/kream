0b8eacaf7d49c25a858e8fc5ebb3b21a1d21276d
kimtaehyun <kth6301@naver.com>
2021-07-16 21:52:57 +0900

    Create MemberService.java

---
+++ kream/src/main/java/com/project/service/MemberService.java
@@ -0,0 +1,18 @@
+package com.project.service;
+
+import com.project.model.LoginDTO;
+import com.project.model.MemberDTO;
+
+
+public interface MemberService {
+	
+	void register(MemberDTO dto) throws Exception;
+	
+	void naver_register(MemberDTO dto) throws Exception;
+	
+	MemberDTO login(LoginDTO dto) throws Exception;
+
+	int checkEmail(String email) throws Exception;
+
+	
+}
